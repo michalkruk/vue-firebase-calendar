d30543c64cf06db1d696778fc990dbd0bc7db171
michalkruk <michal.k.kruk@gmail.com>
2019-11-17 15:05:51 +0100

    up

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyA8m12Xz_-OyFpxn7zurKggWIOTD2RJJlY',
+    authDomain: 'vue-calendar-1371a.firebaseapp.com',
+    databaseURL: 'https://vue-calendar-1371a.firebaseio.com',
+    projectId: 'vue-calendar-1371a',
+    storageBucket: 'vue-calendar-1371a.appspot.com',
+    messagingSenderId: '71656755978',
+    appId: '1:71656755978:web:6dc1e412229d48a170a0f3'
+});
+
+export const db = firebase.firestore();
 
 new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+    vuetify,
+    render: h => h(App)
+}).$mount('#app');
